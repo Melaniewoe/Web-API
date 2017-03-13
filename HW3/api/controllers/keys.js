3c2cddd4c1b7a60088d75eaf43a5263c56e9bd55
Melanie Woe <melanie_woe@yahoo.com>
2017-03-13 00:36:08 -0600

    Add files via upload

---
+++ HW3/api/controllers/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  "key1": {
+    "data": "309b1e1d650d0eaac9307d6251f766f798eba549a31720a46cf89f1006c9acdd3b1b3c9c99363e9bc96c6f8a69ca7c1d"
+  }
+};
---
+++ HW3/api/controllers/store.js
@@ -0,0 +1,6 @@
+module.exports = {
+	"vault1": {
+		"key": "key1",
+		"data": "907ca8ff9ee29f08af58ac80a8805717ffb6020562c0da79bc8fcd8e00a537ce899e1fdfc1a15160e3b4c0bfd472157712b8c7304cd4803102489a0d80c59504"
+	}
+};
